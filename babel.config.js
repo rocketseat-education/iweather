49fb0f64c12b69f98a84232c387283244b4b9f83
RennanD <rplaykl@gmail.com>
2023-07-20 11:08:34 -0300

    fix: add babel module resolver to alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes',
+            '@libs': './src/libs',
+            '@styles': './src/styles',
+            '@__tests__': './__tests__',
+          }
+        },
+      ],
+    ],
   };
 };
